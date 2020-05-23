591e209dac2413dc34a85d715e87c2dcce0fa574
wulang <1447186835@qq.com>
2020-05-23 23:34:05 +0800

    second

---
+++ src/vip/wulang/algorithm/FibonacciHash.java
@@ -0,0 +1,19 @@
+package vip.wulang.algorithm;
+
+/*
+magic: 1640531527
+magic(double): 2.6544357694972305E9
+magic(long): 2654435769
+magic(int): -1640531527
+ */
+public class FibonacciHash {
+    public static void main(String[] args) {
+        System.out.println("magic: " + 0x61c88647);
+        double magicD = (1L << 32) * ((Math.sqrt(5) - 1) / 2);
+        System.out.println("magic(double): " + magicD) ;
+        long magicL = (long) ((1L << 32) * ((Math.sqrt(5) - 1) / 2));
+        System.out.println("magic(long): " + magicL);
+        int magicI = (int) magicL;
+        System.out.println("magic(int): " + magicI);
+    }
+}
---
+++ src/vip/wulang/algorithm/FibonacciMethod.java
@@ -0,0 +1,21 @@
+package vip.wulang.algorithm;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class FibonacciMethod {
+    public static void main(String[] args) throws Exception {
+        Path path = Paths.get(URI.create("file:///Users/wulang/IdeaProjects/Algorithm/src/vip/wulang/algorithm/FibonacciMethod.txt"));
+        double[] f = new double[Integer.MAX_VALUE >> 16];
+        f[0] = f[1] = 1;
+        for (int i = 2; i < f.length; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+            String str = "当前 " + i + " 项：" + (f[i - 1] / f[i]) + "\r\n";
+            Files.writeString(path, str, Charset.defaultCharset(), StandardOpenOption.APPEND);
+        }
+    }
+}
